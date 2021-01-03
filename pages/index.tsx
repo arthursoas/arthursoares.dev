68a1196a13be28ae4d39539573e070aa6dbfebd5
Arthur Soares <arthurvsp97@gmail.com>
2021-01-03 14:02:41 -0300

    Using classes instead functions to render elements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import React, { ReactNode } from 'react';
 
-function Home(): ReactNode {
-    return (
-        <div>Hello World!</div>
-    )
+class Home extends React.Component {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render(): ReactNode {
+        return (
+            <div>Hello World!</div>
+        )
+    }
 }
 
 export default Home;
